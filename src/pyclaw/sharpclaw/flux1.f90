a98b0dd6c36047060d591303992d16e35ce67cf7
Matthew Emmett <memmett@unc.edu>
2012-02-10 04:08:11 -0500

    Start adding derived types.

--- src/pyclaw/sharpclaw/flux1.f90
+++ src/pyclaw/sharpclaw/flux1.f90
@@ -1,195 +1,204 @@
-! ===================================================================
-subroutine flux1(q1d,dq1d,aux,dt,cfl,t,ixy,num_aux,num_eqn,mx,num_ghost,maxnx)
-! ===================================================================
-!
-!     # Evaluate (delta t) * dq(t)/dt
-!
-!     SharpClaw
-!     Author: David Ketcheson
-!
-!     amdq, apdq, amdq2, apdq2, wave, and s are used locally:
-!
-!     amdq(num_eqn,1-num_ghost:mx+num_ghost) = left-going flux-differences
-!     apdq(num_eqn,1-num_ghost:mx+num_ghost) = right-going flux-differences
-!        e.g. amdq(m,i) = m'th component of A^- \Delta q from i'th Riemann
-!                         problem (between cells i-1 and i).
-!
-!     wave(1-num_ghost:mx+num_ghost, num_eqn, num_waves) = waves from solution of
-!                                           Riemann problems,
-!            wave(m,mw,i) = mth component of jump in q across
-!                           wave in family mw in Riemann problem between
-!                           states i-1 and i.
-!
-!     s(num_waves,1-num_ghost:mx+num_ghost) = wave speeds,
-!            s(mw,i) = speed of wave in family mw in Riemann problem between
-!                      states i-1 and i.
-!
-!     Note that mx must be the size of the patch for the dimension corresponding
-!     to the value of ixy.
-!
-!     t is the time at which we want to evaluate dq/dt, which may not
-!      be the current simulation time
-! ===================================================================
-!
-! Modified: April 26, 2011
-! Authors:  David Ketcheson
-!           Matteo Parsani
-!
-! ===================================================================
+module flux1_module
 
-    USE reconstruct
-    USE workspace
-    USE ClawParams
+  use iso_c_binding
 
-    implicit double precision (a-h,o-z)
+contains
 
-    integer :: num_aux,num_eqn,num_ghost,maxnx,mx
-    double precision :: q1d(num_eqn,1-num_ghost:mx+num_ghost)
-    double precision :: dq1d(num_eqn,1-num_ghost:maxnx+num_ghost)
-    dimension aux(num_aux,1-num_ghost:mx+num_ghost)
-    double precision :: auxl(num_aux,1-num_ghost:mx+num_ghost), auxr(num_aux,1-num_ghost:mx+num_ghost)
-    double precision, intent(out) :: cfl
-    integer, intent(in) :: ixy
-    integer t
+  subroutine flux1(cptr,q1d,dq1d,aux,dt,cfl,t,ixy,num_aux,num_eqn,mx,num_ghost,maxnx) &
+       bind(c, name='flux1')
+    ! ===================================================================
+    !
+    !     # Evaluate (delta t) * dq(t)/dt
+    !
+    !     SharpClaw
+    !     Author: David Ketcheson
+    !
+    !     amdq, apdq, amdq2, apdq2, wave, and s are used locally:
+    !
+    !     amdq(num_eqn,1-num_ghost:mx+num_ghost) = left-going flux-differences
+    !     apdq(num_eqn,1-num_ghost:mx+num_ghost) = right-going flux-differences
+    !        e.g. amdq(m,i) = m'th component of A^- \Delta q from i'th Riemann
+    !                         problem (between cells i-1 and i).
+    !
+    !     wave(1-num_ghost:mx+num_ghost, num_eqn, num_waves) = waves from solution of
+    !                                           Riemann problems,
+    !            wave(m,mw,i) = mth component of jump in q across
+    !                           wave in family mw in Riemann problem between
+    !                           states i-1 and i.
+    !
+    !     s(num_waves,1-num_ghost:mx+num_ghost) = wave speeds,
+    !            s(mw,i) = speed of wave in family mw in Riemann problem between
+    !                      states i-1 and i.
+    !
+    !     Note that mx must be the size of the patch for the dimension corresponding
+    !     to the value of ixy.
+    !
+    !     t is the time at which we want to evaluate dq/dt, which may not
+    !      be the current simulation time
+    ! ===================================================================
+    !
+    ! Modified: April 26, 2011
+    ! Authors:  David Ketcheson
+    !           Matteo Parsani
+    !
+    ! ===================================================================
 
-!f2py intent(in,out) dq1d  
-!f2py intent(out) cfl  
-!f2py optional dq1d
+    use reconstruct
+    use workspace_module
+    implicit none
 
-    if (index_capa.gt.0) then
-        dtdx = dt / (dx(ixy)*aux(index_capa,:))
+    type(c_ptr), intent(in)     :: cptr
+    integer(c_int), intent(in), value :: num_aux, num_eqn, num_ghost, maxnx, mx, ixy
+    real(c_double), intent(in), value :: dt, t
+    real(c_double), intent(in)  ::  q1d(num_eqn,1-num_ghost:mx+num_ghost)
+    real(c_double), intent(out) :: dq1d(num_eqn,1-num_ghost:maxnx+num_ghost)
+    real(c_double), intent(in)  ::   aux(num_aux,1-num_ghost:mx+num_ghost)
+
+    real(c_double) :: auxl(num_aux,1-num_ghost:mx+num_ghost), auxr(num_aux,1-num_ghost:mx+num_ghost)
+    real(c_double), intent(out) :: cfl
+
+    type(workspace), pointer :: wks
+
+    integer :: i, m, mw
+
+    call c_f_pointer(cptr, wks)
+
+    if (wks%index_capa.gt.0) then
+       wks%dtdx = dt / (wks%dx(ixy)*aux(wks%index_capa,:))
     else
-        dtdx = dt/dx(ixy)
+       wks%dtdx = dt/wks%dx(ixy)
     endif
-    if (num_dim.gt.1) dq1d=0.d0
+    if (wks%num_dim.gt.1) dq1d=0.d0
 
 
-    select case(lim_type)
-        ! Non-limited reconstruction of components of q (simplest approach)
-!        case(0)
-!        select case(char_decomp)
-!            case(0)
-!                call q2qlqr_poly(q1d,ql,qr,mx)
-!            case(1)
-!                ! wave-based unlimited reconstruction
-!                call rp1(maxnx,num_eqn,num_waves,num_ghost,mx,&
-!                        q1d,q1d,aux,aux,wave,s,amdq,apdq,num_aux)
-!                call q2qlqr_poly_wave(q1d,ql,qr,wave,s,mx)
-!        end select
-        case(1)
-        select case(char_decomp)
-            case(0)
-                ! TVD reconstruction w/o char. decomp.
-                call tvd2(q1d,ql,qr,mthlim)
-            case(1)
-                ! wave-based second order reconstruction
-                call rp1(maxnx,num_eqn,num_waves,num_ghost,mx,&
-                        q1d,q1d,aux,aux,wave,s,amdq,apdq,num_aux)
-                ! Need to write a tvd2_fwave routine
-                call tvd2_wave(q1d,ql,qr,wave,s,mthlim)
-            case(2)
-                ! characteristic-wise second order reconstruction
-                call evec(mx,num_eqn,num_ghost,mx,q1d,aux,aux,evl,evr)
-                call tvd2_char(q1d,ql,qr,mthlim,evl,evr)
-        end select
-        case(2)
-        select case (char_decomp)
-            case (0)
-                ! no characteristic decomposition
-                call weno_comp(q1d,ql,qr,num_eqn,maxnx,num_ghost)
-            case (1)
-                ! wave-based reconstruction
-                call rp1(maxnx,num_eqn,num_waves,num_ghost,mx,&
-                        q1d,q1d,aux,aux,wave,s,amdq,apdq,num_aux)
-                if (fwave.eqv. .True.) then
-                    call weno5_fwave(q1d,ql,qr,wave,s)
-                else
-                    call weno5_wave(q1d,ql,qr,wave)
-                endif
-            case (2)
-                ! characteristic-wise reconstruction
-                call evec(mx,num_eqn,num_ghost,mx,q1d,aux,aux,evl,evr)
-                call weno5_char(q1d,ql,qr,evl,evr)
-            case (3)
-                ! transmission-based reconstruction
-                call evec(mx,num_eqn,num_ghost,mx,q1d,aux,aux,evl,evr)
-                call weno5_trans(q1d,ql,qr,evl,evr)
-            case default
-                write(*,*) 'ERROR: Unrecognized characteristic decomposition option'
-                write(*,*) 'You should set 0<=char_decomp<=3'
-                stop
-        end select
-        case(3)
-            call weno5(q1d,ql,qr,num_eqn,maxnx,num_ghost)
-      end select
+    select case(wks%lim_type)
+       ! Non-limited reconstruction of components of q (simplest approach)
+       !        case(0)
+       !        select case(char_decomp)
+       !            case(0)
+       !                call q2qlqr_poly(q1d,ql,qr,mx)
+       !            case(1)
+       !                ! wave-based unlimited reconstruction
+       !                call rp1(maxnx,num_eqn,num_waves,num_ghost,mx,&
+       !                        q1d,q1d,aux,aux,wave,s,amdq,apdq,num_aux)
+       !                call q2qlqr_poly_wave(q1d,ql,qr,wave,s,mx)
+       !        end select
+    case(1)
+       select case(wks%char_decomp)
+       case(0)
+          ! TVD reconstruction w/o char. decomp.
+          call tvd2(q1d,wks%ql,wks%qr,wks%mthlim)
+       case(1)
+          ! wave-based second order reconstruction
+          call rp1(maxnx,num_eqn,wks%num_waves,num_ghost,mx,&
+               q1d,q1d,aux,aux,wks%wave,wks%s,wks%amdq,wks%apdq,num_aux)
+          ! Need to write a tvd2_fwave routine
+          call tvd2_wave(q1d,wks%ql,wks%qr,wks%wave,wks%s,wks%mthlim)
+       case(2)
+          ! characteristic-wise second order reconstruction
+          call evec(mx,num_eqn,num_ghost,mx,q1d,aux,aux,wks%evl,wks%evr)
+          call tvd2_char(q1d,wks%ql,wks%qr,wks%mthlim,wks%evl,wks%evr)
+       end select
+    case(2)
+       select case (wks%char_decomp)
+       case (0)
+          ! no characteristic decomposition
+          call weno_comp(q1d,wks%ql,wks%qr,num_eqn,maxnx,num_ghost)
+       case (1)
+          ! wave-based reconstruction
+          call rp1(maxnx,num_eqn,wks%num_waves,num_ghost,mx,&
+               q1d,q1d,aux,aux,wks%wave,wks%s,wks%amdq,wks%apdq,num_aux)
+          if (wks%fwave .eqv. .true.) then
+             call weno5_fwave(q1d,wks%ql,wks%qr,wks%wave,wks%s)
+          else
+             call weno5_wave(q1d,wks%ql,wks%qr,wks%wave)
+          endif
+       case (2)
+          ! characteristic-wise reconstruction
+          call evec(mx,num_eqn,num_ghost,mx,q1d,aux,aux,wks%evl,wks%evr)
+          call weno5_char(q1d,wks%ql,wks%qr,wks%evl,wks%evr)
+       case (3)
+          ! transmission-based reconstruction
+          call evec(mx,num_eqn,num_ghost,mx,q1d,aux,aux,wks%evl,wks%evr)
+          call weno5_trans(q1d,wks%ql,wks%qr,wks%evl,wks%evr)
+       case default
+          write(*,*) 'ERROR: Unrecognized characteristic decomposition option'
+          write(*,*) 'You should set 0<=char_decomp<=3'
+          stop
+       end select
+    case(3)
+       call weno5(q1d,wks%ql,wks%qr,num_eqn,maxnx,num_ghost)
+    end select
 
 
     ! solve Riemann problem at each interface 
     ! -----------------------------------------
-    call rp1(maxnx,num_eqn,num_waves,num_ghost,mx,ql,qr,aux,aux, &
-              wave,s,amdq,apdq,num_aux)
+    call rp1(maxnx,num_eqn,wks%num_waves,num_ghost,mx,wks%ql,wks%qr,aux,aux, &
+         wks%wave,wks%s,wks%amdq,wks%apdq,num_aux)
 
     ! compute maximum wave speed:
     cfl = 0.d0
-    do mw=1,num_waves
-        do i=1,mx+1
-            ! if s>0 use dtdx(i) to compute CFL,
-            ! if s<0 use dtdx(i-1) to compute CFL:
-            cfl = dmax1(cfl, dtdx(i)*s(mw,i), -dtdx(i-1)*s(mw,i))
-        enddo
+    do mw=1,wks%num_waves
+       do i=1,mx+1
+          ! if s>0 use wks%dtdx(i) to compute CFL,
+          ! if s<0 use wks%dtdx(i-1) to compute CFL:
+          cfl = dmax1(cfl, wks%dtdx(i)*wks%s(mw,i), -wks%dtdx(i-1)*wks%s(mw,i))
+       enddo
     enddo
 
     ! Find total fluctuation within each cell
-    if (tfluct_solver.eqv. .True.) then
-        ! tfluct should be a special solver that uses the parameters aux(i)
-        ! to solve a Riemann problem with left state ql(i)
-        ! and right state qr(i), and returns a total fluctuation in amdq2
-        ! NOTE that here amdq2 is really a total fluctuation (should be
-        ! called adq); we do it this way just to avoid declaring more storage
-        call tfluct(ixy,maxnx,num_eqn,num_waves,num_ghost,mx,ql,qr, &
-                     aux,aux,s,amdq2)
+    if (wks%tfluct_solver .eqv. .true.) then
+       ! tfluct should be a special solver that uses the parameters aux(i)
+       ! to solve a Riemann problem with left state ql(i)
+       ! and right state qr(i), and returns a total fluctuation in wks%amdq2
+       ! NOTE that here wks%amdq2 is really a total fluctuation (should be
+       ! called adq); we do it this way just to avoid declaring more storage
+       call tfluct(ixy,maxnx,num_eqn,wks%num_waves,num_ghost,mx,wks%ql,wks%qr, &
+            aux,aux,wks%s,wks%amdq2)
 
-        ! Modify q using fluctuations:
-        ! Note this may not correspond to a conservative flux-differencing
-        ! for equations not in conservation form.  It is conservative if
-        ! adq = f(qr(i)) - f(ql(i)).
+       ! Modify q using fluctuations:
+       ! Note this may not correspond to a conservative flux-differencing
+       ! for equations not in conservation form.  It is conservative if
+       ! adq = f(qr(i)) - f(ql(i)).
 
-        forall (i=1:mx, m=1:num_eqn)
-            dq1d(m,i) = dq1d(m,i) - dtdx(i)*(apdq(m,i) + &
-                            amdq2(m,i) + amdq(m,i+1))
-        end forall
+       forall (i=1:mx, m=1:num_eqn)
+          dq1d(m,i) = dq1d(m,i) - wks%dtdx(i)*(wks%apdq(m,i) + &
+               wks%amdq2(m,i) + wks%amdq(m,i+1))
+       end forall
 
     else
-        ! Or we can just swap things around and use the usual Riemann solver
-        ! This may be more convenient, but is less efficient. 
-        ! For the moment the swapping is done working with the element of the 
-        ! vectors qr, ql, auxl, auxr. 
-        ! 
-        ! TODO: Working with pointers!!!
-        
-        do i = 1-num_ghost+1,mx+num_ghost
-            do m = 1, num_eqn
-                qr(m,i-1) = ql(m,i)
-                ql(m,i  ) = qr(m,i)
-            enddo
-        enddo
+       ! Or we can just swap things around and use the usual Riemann solver
+       ! This may be more convenient, but is less efficient. 
+       ! For the moment the swapping is done working with the element of the 
+       ! vectors qr, ql, auxl, auxr. 
+       ! 
+       ! TODO: Working with pointers!!!
 
-        if (num_aux .gt. 0) then
-             do i = 1-num_ghost+1,mx+num_ghost
-                do m = 1, num_aux
-                    auxr(m,i-1) = aux(m,i) !aux is not patchdat type
-                    auxl(m,i  ) = aux(m,i) !aux is not patchdat type
-                enddo
-            enddo
-        endif
-        
-        call rp1(maxnx,num_eqn,num_waves,num_ghost,mx,ql,qr, &
-                 auxl,auxr,wave,s,amdq2,apdq2,num_aux)
+       do i = 1-num_ghost+1,mx+num_ghost
+          do m = 1, num_eqn
+             wks%qr(m,i-1) = wks%ql(m,i)
+             wks%ql(m,i  ) = wks%qr(m,i)
+          enddo
+       enddo
 
-        forall(i=1:mx, m=1:num_eqn)
-            dq1d(m,i) = dq1d(m,i)-dtdx(i)*(amdq(m,i+1)+ &
-                        apdq(m,i)+amdq2(m,i)+apdq2(m,i))
-        end forall
+       if (num_aux .gt. 0) then
+          do i = 1-num_ghost+1,mx+num_ghost
+             do m = 1, num_aux
+                auxr(m,i-1) = aux(m,i) !aux is not patchdat type
+                auxl(m,i  ) = aux(m,i) !aux is not patchdat type
+             enddo
+          enddo
+       endif
+
+       call rp1(maxnx,num_eqn,wks%num_waves,num_ghost,mx,wks%ql,wks%qr, &
+            auxl,auxr,wks%wave,wks%s,wks%amdq2,wks%apdq2,num_aux)
+
+       forall(i=1:mx, m=1:num_eqn)
+          dq1d(m,i) = dq1d(m,i)-wks%dtdx(i)*(wks%amdq(m,i+1)+ &
+               wks%apdq(m,i)+wks%amdq2(m,i)+wks%apdq2(m,i))
+       end forall
     endif
 
-end subroutine flux1
+  end subroutine flux1
+
+end module flux1_module
--- src/pyclaw/sharpclaw/workspace.f90
+++ src/pyclaw/sharpclaw/workspace.f90
@@ -1,54 +1,89 @@
-module workspace
-    double precision, allocatable  :: ql(:,:), qr(:,:), dtdx(:)
-    double precision, allocatable  :: evl(:,:,:), evr(:,:,:)
-    double precision, allocatable  :: amdq(:,:), apdq(:,:), amdq2(:,:), apdq2(:,:)
-    double precision, allocatable  :: wave(:,:,:), s(:,:)
-    logical :: work_alloc = .False.
+module workspace_module
+
+  use iso_c_binding
+
+  type :: workspace
+     
+     integer(c_int) :: num_dim, num_waves, index_capa
+     integer(c_int) :: char_decomp, lim_type, multid_recon, weno_order
+     logical :: fwave, tfluct_solver
+
+     real(c_double), pointer :: ql(:,:), qr(:,:), dtdx(:)
+     real(c_double), pointer :: evl(:,:,:), evr(:,:,:)
+     real(c_double), pointer :: amdq(:,:), apdq(:,:), amdq2(:,:), apdq2(:,:)
+     real(c_double), pointer :: wave(:,:,:), s(:,:)
+
+     real(c_double), pointer :: xlower(:), xupper(:), dx(:)
+     integer(c_int), pointer :: mthlim(:)
+
+     logical :: work_alloc = .false.
+     
+  end type workspace
 
 contains
+  
+  subroutine alloc_workspace(cptr,maxnx,num_ghost,num_dim,num_eqn,num_waves,char_decomp) &
+       bind(c, name='alloc_workspace')
 
-    subroutine alloc_workspace(maxnx,num_ghost,num_eqn,num_waves,char_decomp)
-        integer,intent(in) :: maxnx,num_ghost,num_eqn,num_waves,char_decomp
+    type(c_ptr), intent(out) :: cptr
+    integer(c_int), intent(in), value :: maxnx,num_ghost,num_dim,num_eqn,num_waves,char_decomp
+    type(workspace), pointer :: wkspace
 
-        allocate(ql(num_eqn,1-num_ghost:maxnx+num_ghost))
-        allocate(qr(num_eqn,1-num_ghost:maxnx+num_ghost))
-        allocate(amdq(num_eqn,1-num_ghost:maxnx+num_ghost))
-        allocate(apdq(num_eqn,1-num_ghost:maxnx+num_ghost))
-        allocate(amdq2(num_eqn,1-num_ghost:maxnx+num_ghost))
-        allocate(apdq2(num_eqn,1-num_ghost:maxnx+num_ghost))
-        allocate(wave(num_eqn,num_waves,1-num_ghost:maxnx+num_ghost))
-        allocate(s(num_waves,1-num_ghost:maxnx+num_ghost))
-        allocate(dtdx(1-num_ghost:maxnx+num_ghost))
+    allocate(wkspace)
+    cptr = c_loc(wkspace)
 
-        if (char_decomp>1) then
-            allocate(evl(num_eqn,num_eqn,1-num_ghost:maxnx+num_ghost))
-            allocate(evr(num_eqn,num_eqn,1-num_ghost:maxnx+num_ghost))
-        endif
+    wkspace%char_decomp = char_decomp
+    wkspace%num_dim = num_dim
+    wkspace%num_waves = num_waves
 
-        work_alloc = .True.
+    allocate(wkspace%ql(num_eqn,1-num_ghost:maxnx+num_ghost))
+    allocate(wkspace%qr(num_eqn,1-num_ghost:maxnx+num_ghost))
+    allocate(wkspace%amdq(num_eqn,1-num_ghost:maxnx+num_ghost))
+    allocate(wkspace%apdq(num_eqn,1-num_ghost:maxnx+num_ghost))
+    allocate(wkspace%amdq2(num_eqn,1-num_ghost:maxnx+num_ghost))
+    allocate(wkspace%apdq2(num_eqn,1-num_ghost:maxnx+num_ghost))
+    allocate(wkspace%wave(num_eqn,num_waves,1-num_ghost:maxnx+num_ghost))
+    allocate(wkspace%s(num_waves,1-num_ghost:maxnx+num_ghost))
+    allocate(wkspace%dtdx(1-num_ghost:maxnx+num_ghost))
 
-    end subroutine alloc_workspace
+    if (wkspace%char_decomp>1) then
+       allocate(wkspace%evl(num_eqn,num_eqn,1-num_ghost:maxnx+num_ghost))
+       allocate(wkspace%evr(num_eqn,num_eqn,1-num_ghost:maxnx+num_ghost))
+    endif
 
-    subroutine dealloc_workspace(char_decomp)
+    allocate(wkspace%xlower(num_dim))
+    allocate(wkspace%xupper(num_dim))
+    allocate(wkspace%dx(num_dim))
+    allocate(wkspace%mthlim(num_waves))
 
-        deallocate(ql)
-        deallocate(qr)
-        deallocate(amdq)
-        deallocate(apdq)
-        deallocate(amdq2)
-        deallocate(apdq2)
-        deallocate(wave)
-        deallocate(s)
-        deallocate(dtdx)
+    wkspace%work_alloc = .true.
 
-        if (char_decomp>1) then
-            deallocate(evl)
-            deallocate(evr)
-        endif
+  end subroutine alloc_workspace
 
-        work_alloc = .False.
+  subroutine dealloc_workspace(cptr) bind(c, name='dealloc_workspace')
 
-    end subroutine dealloc_workspace
+    type(c_ptr), intent(in) :: cptr
+    type(workspace), pointer :: wkspace
 
+    call c_f_pointer(cptr, wkspace)
 
-end module workspace
+    deallocate(wkspace%ql)
+    deallocate(wkspace%qr)
+    deallocate(wkspace%amdq)
+    deallocate(wkspace%apdq)
+    deallocate(wkspace%amdq2)
+    deallocate(wkspace%apdq2)
+    deallocate(wkspace%wave)
+    deallocate(wkspace%s)
+    deallocate(wkspace%dtdx)
+
+    if (wkspace%char_decomp>1) then
+       deallocate(wkspace%evl)
+       deallocate(wkspace%evr)
+    endif
+
+    wkspace%work_alloc = .false.
+
+  end subroutine dealloc_workspace
+
+end module workspace_module
